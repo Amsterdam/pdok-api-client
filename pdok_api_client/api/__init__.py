063ee3338ec8fad6f8ac8aaa3a86a0e4b963d942
ramonavic <r.hofmeister@amsterdam.nl>
2025-09-05 15:11:12 +0200

    update dependencies

--- pdok_api_client/api/__init__.py
+++ pdok_api_client/api/__init__.py
@@ -1,5 +1,20 @@
 # flake8: noqa
 
-# import apis into api package
+if __import__("typing").TYPE_CHECKING:
+    # import apis into api package
+    from pdok_api_client.api.locatieserver_api import LocatieserverApi
+    
+else:
+    from lazy_imports import LazyModule, as_package, load
+
+    load(
+        LazyModule(
+            *as_package(__file__),
+            """# import apis into api package
 from pdok_api_client.api.locatieserver_api import LocatieserverApi
 
+""",
+            name=__name__,
+            doc=__doc__,
+        )
+    )
