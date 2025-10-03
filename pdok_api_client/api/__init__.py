64356fa848f6f708c75c50254b6bfae76b5b5739
ramonavic <r.hofmeister@amsterdam.nl>
2025-10-03 16:05:26 +0200

    update to 7.16 to fix lazy import issue

--- pdok_api_client/api/__init__.py
+++ pdok_api_client/api/__init__.py
@@ -1,20 +1,5 @@
 # flake8: noqa
 
-if __import__("typing").TYPE_CHECKING:
-    # import apis into api package
-    from pdok_api_client.api.locatieserver_api import LocatieserverApi
-    
-else:
-    from lazy_imports import LazyModule, as_package, load
-
-    load(
-        LazyModule(
-            *as_package(__file__),
-            """# import apis into api package
+# import apis into api package
 from pdok_api_client.api.locatieserver_api import LocatieserverApi
 
-""",
-            name=__name__,
-            doc=__doc__,
-        )
-    )
